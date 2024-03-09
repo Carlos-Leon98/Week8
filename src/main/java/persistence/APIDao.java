855eb14d1358c97b8569b90686b57386aa7ca4c3
Carlos-Leon98 <carlosaleonrivas@gmail.com>
2024-03-09 17:46:44 -0600

    Adding properties file

--- src/main/java/persistence/APIDao.java
+++ src/main/java/persistence/APIDao.java
@@ -4,16 +4,41 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.swapi.Response;
 
+import java.io.IOException;
+import java.util.Properties;
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
 
 public class APIDao {
 
+    private Properties properties;
+
+    public APIDao() {
+        loadProperties();
+    }
+
+    private void loadProperties() {
+        properties = new Properties();
+        try {
+            properties.load (this.getClass().getResourceAsStream("/API.properties"));
+        } catch (IOException ioe) {
+            System.out.println("Database.loadProperties()...Cannot load the properties file");
+            ioe.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Database.loadProperties()..." + e);
+            e.printStackTrace();
+        }
+
+    }
+
     public Response getProductById(int id) {
+        String url = properties.getProperty("productsURL");
         Client client = ClientBuilder.newClient();
         WebTarget target =
-                client.target("https://api.escuelajs.co/api/v1/products/" + id);
-        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+                client.target(url + id);
+        String response = target
+                .request(MediaType.APPLICATION_JSON)
+                .get(String.class);
 
         ObjectMapper mapper = new ObjectMapper();
         Response product = null;
